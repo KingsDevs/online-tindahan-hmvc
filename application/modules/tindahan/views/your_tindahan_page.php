8ff5ba644e9151875b69a140374fb46f3895a157
KingsDevs <sunnyckair@gmail.com>
2021-08-10 02:39:57 +0800

    wait for it

--- application/modules/tindahan/views/your_tindahan_page.php
+++ application/modules/tindahan/views/your_tindahan_page.php
@@ -24,21 +24,27 @@
                 Your Tindahan List
             </div>
             <div class="scroll-list">
-                
-                <div class="card mb-3 tindahan-list">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                        <img src="..." class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+                <?php if($this->session->has_userdata('tindahan_data')) : ?>
+                            <?php 
+                                $tindahan_data = $this->session->userdata('tindahan_data');
+                                foreach($tindahan_data as $td) :
+                            ?>
+                    <div class="card mb-3 tindahan-list">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                <?php endforeach ; ?>
+            <?php endif ; ?>
             </div>
         </div>
 
