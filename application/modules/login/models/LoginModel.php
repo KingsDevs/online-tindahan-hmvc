2ee48fd05130eaf6f2e7f38b2b45c20e80548f75
KingsDevs <sunnyckair@gmail.com>
2021-08-09 15:13:06 +0800

    change the database for loggin in

--- application/modules/login/models/LoginModel.php
+++ application/modules/login/models/LoginModel.php
@@ -2,21 +2,29 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class LoginModel extends CI_Model
 {
+    private $user_db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->user_db = $this->load->database('online_tindahan_users', TRUE);
+    }
+
     public function insert_user($data)
     {
         $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
         
-        return $this->db->insert('users', $data);
+        return $this->user_db->insert('users', $data);
     }
 
     public function login_user($data)
     {
-        $this->db->select('*');
-        $this->db->from('users');
-        $this->db->where('username' , $data['username']);
-        $this->db->limit(1);
+        $this->user_db->select('*');
+        $this->user_db->from('users');
+        $this->user_db->where('username' , $data['username']);
+        $this->user_db->limit(1);
 
-        $query = $this->db->get();
+        $query = $this->user_db->get();
         if($query->num_rows() == 1)
         {
             if(password_verify($data['password'], $query->row()->password))
